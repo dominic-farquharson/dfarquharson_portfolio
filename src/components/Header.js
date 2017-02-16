53d40505a388983ad24614afe6bc6327c454db4b
Dominic Farquharson <farquharsondominic@gmail.com>
2017-02-16 00:34:31 -0500

    Updating about component

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,14 +7,30 @@ let Header = (props) => {
   return (
     <header>
       <nav>
-        <ul>
-          <span><li>DF</li></span>
-          <li><a href="#">Home</a></li>
-          <li><a href="#about">About</a></li>
-          <li><a href="#projects">Projects</a></li>
+        {/* Navbar for devices greater than 960px */}
+        <ul className="uk-visible@m">
+          {/* Aligining logo to the left */}
+          <span className="uk-position-left"><li>DF</li></span>
+          {/* Aligining nav links to the right */}
+          <section className="uk-position-right">
+            <li><a href="#">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#projects">Projects</a></li>
+          </section>
         </ul>
-      </nav>
 
+        {/* Navbar for devices less than 960px */}
+
+      </nav>
+      {/* vertical/horizontally aligning section using uikit */}
+      <section>
+        <p>
+          <h1 className="uk-position-center">Dominic Farquharson
+            <br />
+            Web Developer
+          </h1>
+        </p>
+      </section>
     </header>
   )
 }
