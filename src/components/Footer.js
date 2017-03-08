c34e10e838f8ebfcee86f6c3681c784922433a33
dominic-farquharson <farquharsondominic@gmail.com>
2017-03-08 00:21:48 -0500

    Adding Social links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,6 +5,20 @@ import React, {Component} from 'react';
 let Footer = (props) => {
     return (
       <footer>
+        <ul id="socialLinks">
+          <li>
+            {/* github - link */}
+            <a style={{color:"white"}} href="https://github.com/dominic-farquharson">
+              <i className="fa fa-github fa-3x" aria-hidden="true"></i>
+            </a>
+          </li>
+          <li>
+            {/* Linkedin Link */}
+            <a style={{color:"white"}} href="https://www.linkedin.com/in/dominic-farquharson/">
+              <i className="fa fa-linkedin-square fa-3x" aria-hidden="true"></i>
+            </a>
+          </li>
+        </ul>  
         <h1>&copy; Dominic Farquaharson 2017</h1>
       </footer>
     )
