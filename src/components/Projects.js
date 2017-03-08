ea846f5ebcc020edd8635b01ead3ecfd3201cac9
dominic-farquharson <farquharsondominic@gmail.com>
2017-03-07 23:53:24 -0500

    Fixing merge conflicts. Cleaning up code

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -37,7 +37,6 @@ class Projects extends Component {
       // Printing responsive cards to hold projects
       Object.keys(projects).map( (project, key)=>{
         return(
-<<<<<<< HEAD
           /*
             Card containing projects
           */  
@@ -48,31 +47,6 @@ class Projects extends Component {
               <div className="uk-card-body">
                 {/* Project Image */}
                 <img src={projects[project]['image']}  />
-=======
-        <section className="portfolioCard projects" key={key}>
-          <h1>{projects[project]['name']}</h1>
-          <div className="uk-card uk-card-default uk-grid-collapse uk-child-width-1-2@s uk-margin" data-uk-grid>
-              <div className="uk-card-media-left uk-cover-container">
-                  <img className="portfolioImage" title={projects[project]['title']} src={projects[project]['image']} alt={projects[project]['alt']} data-uk-cover />
-              </div>
-              <div>
-                  <div className="uk-card-body">
-                      <h3 className="uk-card-title">{projects[project]['name']}</h3>
-                      <br />
-                      {/* Getting description and technology used */}
-                      <p>
-                        <span>Description:</span>
-                        {projects[project]['description']}
-                        <br /><br />
-                        <span>Built with:</span> {projects[project]['technology']}
-                      </p>
-                      {/* link to site and Repo buttons  */}
-                      <section className="siteRepoLinks">
-                        <a className="btn btn-default" href={projects[project]['link']}>Link to site</a>
-                        <a className="btn btn-default" href={projects[project]['repo']}>Link to repo</a>
-                      </section>
-                  </div>
->>>>>>> cbfc605c124b9b66d81f77a9d20afecffa469fe2
               </div>
               <div className="uk-card-footer">
                   <a className="btn btn-default" href={projects[project]['link']}>Visit Site</a>
