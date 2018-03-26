d2f9091e32705a1fe691453beea625d2b4bc0387
Dominic Farquharson <farquharsondominic@gmail.com>
2018-03-26 17:10:21 -0400

    add routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import './App.css';
 // importing components
-import Header from './components/Header';
+import Home from './components/Home';
 import Footer from './components/Footer';
 import Projects from './components/Projects';
 import About from './components/About';
@@ -53,20 +54,55 @@ class App extends Component {
     return (
       <div className="App">
         <main>
-          <Header />
-          <div id="container">
-            {/* Setting skilsl prop to skills state, will be pulled from firebase - allow for server side updating of skills*/}
-            <About
-              skills={this.state.skills}
-            />
-            {/* Setting projects prop to projects state, also pulled from firebase */}
-            <Projects
-              projects={this.state.projects}
-            />
-            {/* Contact Component */}
-            <Contact />
-            <Footer />
-          </div>
+          <Router>
+            <Switch>
+              <div>
+                <Navbar />
+                <Route
+                  exact path="/"
+                  render={props => {
+                    return (
+                      <Home /> 
+                    );
+                  }}
+                />
+
+                {/* Setting skilsl prop to skills state, will be pulled from firebase - allow for server side updating of skills*/}
+                <Route 
+                  exact path="/about"
+                  render={props => {
+                    return (
+                      <About
+                        skills={this.state.skills}
+                      />
+                    );
+                  }}
+                />
+                <Route 
+                  exact path="/projects"
+                  render={props => {
+                    {/* Setting projects prop to projects state, also pulled from firebase */}
+                    return (
+                      <Projects
+                        projects={this.state.projects}
+                      />
+                    );
+                  }}
+                />
+
+                <Route 
+                  exact path="/contact"
+                  render={props => {
+                    return (
+                      <Contact />
+                    );
+                  }}
+                />
+                
+                {/* <Footer /> */}
+              </div>
+            </Switch>
+          </Router>
         </main>
       </div>
 
@@ -74,4 +110,31 @@ class App extends Component {
   }
 }
 
+function Navbar() {
+  return (  
+    <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+      <Link className="navbar-brand" to="/">DF</Link>
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+        <ul className="nav navbar-right navbar-nav justify-content-center ml-md-auto">
+          <li className="nav-item"> 
+            <Link className="nav-link" to="/about">About</Link>  
+          </li> 
+          <li className="nav-item"> 
+            <Link className="nav-link" to="/projects">Projects</Link>  
+          </li> 
+          <li className="nav-item"> 
+            <Link className="nav-link" to="/contact">Contact</Link>  
+          </li> 
+        </ul>
+      </div>
+    </nav>
+  )
+}
+
+
 export default App;
+
+
