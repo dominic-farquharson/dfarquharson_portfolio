29ae88104d7326b2b97eadc05b8d9fd3d32437c7
Dominic Farquharson <farquharsondominic@gmail.com>
2018-03-26 18:35:16 -0400

    add new about layout

--- src/components/About.js
+++ src/components/About.js
@@ -16,28 +16,49 @@ const About = (props) => {
   }
 
   return (
-    <div id="about">
+    <div className="container">
       <h2>About</h2>
-      <div className="uk-column-1-2@m">
-        <div className="uk-card uk-card-default uk-card-body">
-          <img title="Picture of Dominc Farquharson" alt="Picture of Dominic Farquharson." id="aboutImage" src="/assets/images/me 2014.jpg"/>
+      <div className="row justify-content-center"> 
+
+        <div style={{height: '75vh'}}>
+          {/* <div className="card col-lg-5 col-md-6 col-sm-12"> */}
+          <img className="img-fluid img-thumbnail h-100" title="Picture of Dominc Farquharson" alt="Picture of Dominic Farquharson." src="/assets/images/me 2014.jpg"/>
         </div>
-        {/* About section */}
-        <div className="about uk-card uk-card-default uk-card-body">
-          <h3 className="uk-card-title">Hi, I'm Dominic.
-            <br/>
-            <span className="subText">
-              But you can call me Dom.
-            </span>
-          </h3>
-          <p>
-            I am currently working as an Instructional Associate for the WDI program at General Assembly. Web developement has always been an interest of mine.I was born and raised in the Bronx, NY. When I am not hunched over a computer screen, I enjoy reading and writing. My favorite book series is Wheel of Time. In my spare time I am also writing a fiction novel that I hope to complete by the end of the year.
-          </p>
-        </div>
+        <div className="card col-lg-5 col-md-6 col-sm-12">
+          <div className="card-body">
+            <p style={{fontSize: '1.5rem', lineHeight: 1.4}}className="text-center">Hi, I'm Dominic.
+              <br/>
+              <span className="subText font-italic">
+                But you can call me Dom.
+              </span>
+            </p>
+            <p>
+              I am currently working as an Instructional Associate for the WDI program at General Assembly. Web developement has always been an interest of mine.I was born and raised in the Bronx, NY. When I am not hunched over a computer screen, I enjoy reading and writing. My favorite book series is Wheel of Time. In my spare time I am also writing a fiction novel that I hope to complete by the end of the year.
+            </p>
+            </div>
+        </div>      
       </div>
     </div>
-  )
+  );
 }
 
 // exporting component
 export default About;
+
+// {/* <div className="uk-column-1-2@m">
+// <div className="uk-card uk-card-default uk-card-body">
+//   <img title="Picture of Dominc Farquharson" alt="Picture of Dominic Farquharson." id="aboutImage" src="/assets/images/me 2014.jpg"/>
+// </div>
+// {/* About section */}
+// <div className="about uk-card uk-card-default uk-card-body">
+//   <h3 className="uk-card-title">Hi, I'm Dominic.
+//     <br/>
+//     <span className="subText">
+//       But you can call me Dom.
+//     </span>
+//   </h3>
+//   <p>
+//     I am currently working as an Instructional Associate for the WDI program at General Assembly. Web developement has always been an interest of mine.I was born and raised in the Bronx, NY. When I am not hunched over a computer screen, I enjoy reading and writing. My favorite book series is Wheel of Time. In my spare time I am also writing a fiction novel that I hope to complete by the end of the year.
+//   </p>
+// </div>
+// </div> */}
--- src/components/Home.js
+++ src/components/Home.js
@@ -6,18 +6,20 @@ import '../css/style.css';
 // stateless header component
 const Home = (props) => {
   return (
-    <div className="container">
-      {/* accounting for padding */}
-      <section className=" row align-items-center" style={{height: "calc(100vh - 98px)"}}>
-        <div className="col col align-self-center">
-          <h1>Dominic Farquharson
-            <br />
-            <span id="sub">Web Developer</span>
-          </h1>
-        </div>
-      </section>
+    <div className="site-background" style={{position: 'absolute', width: '100vw', height: '100vh', top: 0, right: 0}}>
+      <div className="container">
+        {/* accounting for padding */}
+        <section className=" row align-items-center" style={{height: "calc(100vh - 98px)"}}>
+          <div className="col col align-self-center">
+            <h1 style={{color: 'white'}}>Dominic Farquharson
+              <br />
+              <span id="sub">Web Developer</span>
+            </h1>
+          </div>
+        </section>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Home;
