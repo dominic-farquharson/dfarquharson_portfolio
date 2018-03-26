2613ed71ff5b1d430716818435f8f0d73758e414
Dominic Farquharson <farquharsondominic@gmail.com>
2018-03-26 17:09:46 -0400

    update home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,16 +6,18 @@ import '../css/style.css';
 // stateless header component
 const Home = (props) => {
   return (
-    <div>
-      <section>
-          <h1 className="align-middle">Dominic Farquharson
+    <div className="container">
+      {/* accounting for padding */}
+      <section className=" row align-items-center" style={{height: "calc(100vh - 98px)"}}>
+        <div className="col col align-self-center">
+          <h1>Dominic Farquharson
             <br />
             <span id="sub">Web Developer</span>
           </h1>
+        </div>
       </section>
     </div>
   )
 }
-
 
 export default Home;
